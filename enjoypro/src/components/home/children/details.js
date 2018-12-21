e2e3bc4fea0f0dcd511300db120174820ffab736
flora32 <flora32@163.com>
2018-12-21 20:09:12 +0800

    第二天dc

---
+++ enjoypro/src/components/home/children/details.js
@@ -0,0 +1,12 @@
+import React,{Component} from "react";
+
+export default class Default extends Component{
+    render(){
+        let {} = this.props;
+        return (
+            <div>
+                详情页
+            </div>
+        )
+    }
+}
--- enjoypro/src/components/home/children/firlist.js
+++ enjoypro/src/components/home/children/firlist.js
@@ -1,15 +1,42 @@
-import React,{Component} from "react";
-
+import React,{Component,Fragment} from "react";
+import {Route} from "react-router-dom";
+import {Link} from "react-router-dom";
+import {Switch} from "react-router-dom";
+import Details from "./details"
 export default class firList extends Component{
     render(){
         let {dataList}=this.props
         console.log(dataList)
         return (
-            <div className="firList">
-                <h3>每周精选餐饮</h3>
-                <div>几年几号更新</div>
-                
-            </div>
+            <Fragment>
+                <Switch>
+                    <Route path="/details" component={Details} />
+                </Switch>
+                <div className="firList">
+                    {
+                        dataList.map((item,index)=>{
+                            return <div key={index} className="list">
+                                    <h3>{item.group_section.title}</h3>
+                                    <div className="tit">{item.group_section.desc}</div>
+                                    <ul className="list_dat">
+                                        {
+                                            item.tabs.map((items,index)=>{
+                                                return <li key={index}>
+                                                            <Link to="/details">
+                                                                    <img src={items.url} alt=""/>
+                                                                    <div>{items.title}</div>
+                                                                    <span>{items.desc}</span>
+                                                            </Link>
+                                                        </li>
+                                            })
+                                        }
+                                        
+                                    </ul>
+                                </div>
+                        })
+                    }
+                </div>
+            </Fragment>
         )
     }
     
